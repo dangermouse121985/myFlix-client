9ffb0c68c006f217a7c7cd76d1198337a87677b2
David <618375+dangermouse121985@users.noreply.github.com>
2023-11-15 10:33:54 -0500

    Changed Card Favorites Button to a Star

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -53848,13 +53848,12 @@ const MovieCard = ({ movie })=>{
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.ToggleButton), {
                 id: movie.title,
-                className: "mb-2 movie-card--favorites-button",
+                className: ` movie-card--favorites-button ${checked ? "active" : " "} mb-2`,
                 type: "checkbox",
                 variant: "outline-primary",
                 checked: checked,
-                value: "1",
-                onChange: (e)=>setChecked(e.currentTarget.checked),
-                onClick: (event)=>{
+                onChange: ()=>{
+                    setChecked(!checked);
                     user = JSON.parse(localStorage.getItem("user"));
                     if (!user.favorites) return;
                     else if (user.favorites.indexOf(movie.id) > -1) {
@@ -53864,7 +53863,25 @@ const MovieCard = ({ movie })=>{
                         if (window.location.pathname === "/user/favorites") parent.parentNode.removeChild(parent);
                     } else addFav();
                 },
-                children: "Favorite"
+                children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("svg", {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    width: "25",
+                    height: "25",
+                    fill: "currentColor",
+                    class: "bi bi-star-fill",
+                    viewBox: "0 0 16 16",
+                    children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("path", {
+                        d: "M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+                    }, void 0, false, {
+                        fileName: "src/compnents/movie-card/movie-card.jsx",
+                        lineNumber: 104,
+                        columnNumber: 11
+                    }, undefined)
+                }, void 0, false, {
+                    fileName: "src/compnents/movie-card/movie-card.jsx",
+                    lineNumber: 96,
+                    columnNumber: 9
+                }, undefined)
             }, void 0, false, {
                 fileName: "src/compnents/movie-card/movie-card.jsx",
                 lineNumber: 71,
--- src/compnents/movie-card/movie-card.jsx
+++ src/compnents/movie-card/movie-card.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { Button, ToggleButton, Card } from 'react-bootstrap';
+import { Button, ToggleButton, Card, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 //import { setUserProfile } from '../../redux/reducers/user';
@@ -70,13 +70,14 @@ export const MovieCard = ({ movie }) => {
       </Link>
       <ToggleButton
         id={movie.title}
-        className="mb-2 movie-card--favorites-button"
+        className={` movie-card--favorites-button ${
+          checked ? 'active' : ' '
+        } mb-2`}
         type="checkbox"
         variant="outline-primary"
         checked={checked}
-        value="1"
-        onChange={(e) => setChecked(e.currentTarget.checked)}
-        onClick={(event) => {
+        onChange={() => {
+          setChecked(!checked);
           user = JSON.parse(localStorage.getItem('user'));
           if (!user.favorites) {
             return;
@@ -92,7 +93,16 @@ export const MovieCard = ({ movie }) => {
           }
         }}
       >
-        Favorite
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          class="bi bi-star-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+        </svg>
       </ToggleButton>
     </Card>
   );
